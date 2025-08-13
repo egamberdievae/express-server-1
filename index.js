1927cb6fdeaed91650a8890ba1eae487af09e3eb
egamberdievae <egamberdiyevae2@gmail.com>
2025-08-13 12:18:07 +0500

    express server added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Super Server")
+console.log("Super Server");
+const express = require("express");
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.send({ message: "Ok" });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
